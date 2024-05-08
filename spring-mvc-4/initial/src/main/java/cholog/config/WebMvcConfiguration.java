acb52292e6436fcf86c11a0417640824dd6b4aab
Zeus6768 <akaps6768@gmail.com>
2024-05-08 16:12:17 +0900

    mvc-4 done

--- spring-mvc-4/initial/src/main/java/cholog/config/WebMvcConfiguration.java
+++ spring-mvc-4/initial/src/main/java/cholog/config/WebMvcConfiguration.java
@@ -1,27 +1,31 @@
 package cholog.config;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import java.util.List;
+import cholog.ui.AuthenticationPrincipalArgumentResolver;
+import cholog.ui.CheckLoginInterceptor;
 
 @Configuration
 public class WebMvcConfiguration implements WebMvcConfigurer {
-    // TODO: "/" 요청 시 hello.html 페이지 응답하기
+
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("hello");
     }
 
-    // TODO: "/admin/**" 요청 시 LoginInterceptor 동작하게 하기
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new CheckLoginInterceptor()).addPathPatterns("/admin/**");
     }
 
-    // TODO: AuthenticationPrincipalArgumentResolver 등록하기
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(new AuthenticationPrincipalArgumentResolver());
     }
 }
